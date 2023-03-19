5421158c0b0ffaae8c42dbecf724bb2b584a763f
toytronic22 <leshaklew@gmail.com>
2023-03-19 20:54:12 +0300

    1st day

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -4,21 +4,28 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class HomePageTest {
 
     @BeforeAll
     public static void setUp() {
         Configuration.baseUrl = "https://dodopizza.ru/moscow";
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.remote = System.getProperty("remote");
     }
 
     @Test
-    public void checkTitle() {
-        open("/");
-        $("title").shouldBe(visible).shouldHave(
-                com.codeborne.selenide.conditions.Text.text("Dodo Pizza – доставка пиццы на дом и в офис в Москве и Санкт-Петербурге"));
+    public void openPage() {
+        open("https://dodopizza.ru/moscow");
+    }
+
+    public void verifyLocality() {
+        $(byClassName("header__about-slogan-text_locality")).shouldHave(text("Москва"));
     }
 }
