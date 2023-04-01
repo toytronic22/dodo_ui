3e5823e2714a700d13943a795698f54d048a3527
toytronic22 <leshaklew@gmail.com>
2023-04-01 16:21:52 +0300

    5th day

--- src/test/java/tests/ProductPageTest.java
+++ src/test/java/tests/ProductPageTest.java
@@ -1,38 +1,23 @@
 package tests;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.ProductPage;
 
-import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class ProductPageTest extends TestBase {
 
-//    @BeforeAll
-//    public static void setUp() {
-//        baseUrl = "https://dodopizza.ru/moscow";
-//        browserSize = System.getProperty("browserSize", "1920x1080");
-//        browser = System.getProperty("browser", "chrome");
-//        browserVersion = System.getProperty("browserVersion", "200.0");
-//        remote = System.getProperty("remote");
-//        timeout = 30000;
-//        SelenideLogger.addListener("allure", new AllureSelenide());
-//    }
-
     @Test
     @Tag(value = "dodo")
     @DisplayName("Check Localization and Main Menu")
     public void addToCartTest() {
-        step("Открытие главной страницы", () -> {
-            open("");
+        step("Открытие страницы продукта", () -> {
+            ProductPage productPage = open("/pizza/syrnaya-pizza", ProductPage.class);
         });
-        ProductPage productPage = open("/pizza/syrnaya-pizza", ProductPage.class);
+        ProductPage productPage = new ProductPage();
         productPage.checkProductTitleExists();
         productPage.addToCart();
         productPage.checkAddedProductTitle("Сырная");
