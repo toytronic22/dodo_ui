ab1b0f7c6ac0e83f90daa153357ff1cc9094607c
toytronic22 <leshaklew@gmail.com>
2023-04-02 21:01:38 +0300

    6th day

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,14 +2,13 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import config.CredentialsConfig;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
--- src/test/resources/config/CredentialsConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.aeonbits.owner.Config;
-
-@Config.Sources("classpath:config/credentials.properties")
-public interface CredentialsConfig extends Config {
-    String selenoidLogin();
-    String selenoidPass();
-}
