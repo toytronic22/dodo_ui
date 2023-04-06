0cfd86afd588779190d44fe05ef2be3a77a9e583
toytronic22 <leshaklew@gmail.com>
2023-04-06 18:04:03 +0300

    7th day

--- src/test/resources/CredentialsConfig.java
+++
@@ -1,6 +0,0 @@
-import org.aeonbits.owner.Config;
-@Config.Sources("classpath:config/credentials.properties")
-public interface CredentialsConfig extends Config {
-    String selenoidLogin();
-    String selenoidPass();
-}
