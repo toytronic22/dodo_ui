a8350acd370184997451fa95ebfb8e0ac0888d07
toytronic22 <leshaklew@gmail.com>
2023-04-06 17:57:51 +0300

    7th day

--- src/test/java/ru/dodopizza/tests/HomePageTest.java
+++ src/test/java/ru/dodopizza/tests/HomePageTest.java
@@ -1,21 +1,21 @@
 package ru.dodopizza.tests;
 
 import io.qameta.allure.Feature;
-import io.qameta.allure.Link;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import ru.dodopizza.pages.HomePage;
 
 import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Owner("Alexey Martynov")
-@Feature("Задачи в репозитории")
-@Story("Просмотр созданных задач в репозитории")
-@Link(value = "Тест dodo_ui", url = "https://github.com/toytronic22/dodo_ui")
+@Owner("toytronic")
+@Feature("UI тесты dodopizza.ru")
+@Story("Главная страница dodopizza.ru")
+@Tags({@Tag("ui")})
 
 public class HomePageTest extends TestBase {
     HomePage homePage = new HomePage();
--- src/test/java/ru/dodopizza/tests/ProductPageTest.java
+++ src/test/java/ru/dodopizza/tests/ProductPageTest.java
@@ -1,7 +1,8 @@
 package ru.dodopizza.tests;
 
 import io.qameta.allure.Feature;
-import io.qameta.allure.Link;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
@@ -13,17 +14,16 @@ import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-@Owner("Alexey Martynov")
-@Feature("Задачи в репозитории")
-@Story("Просмотр созданных задач в репозитории")
-@Link(value = "Тест dodo_ui", url = "https://github.com/toytronic22/dodo_ui")
+@Owner("toytronic")
+@Feature("UI тесты dodopizza.ru")
+@Story("Главная страница lamoda.ru")
+@Tags({@Tag("ui")})
 
 public class ProductPageTest extends TestBase {
     ProductPage productPage = new ProductPage();
 
     @Test
     @DisplayName("Check Adding Exact Product to Cart")
-    @Tag("dodo")
     public void addToCartTest() {
         step("Открытие страницы - https://dodopizza.ru/moscow", () -> {
             open(baseUrl);
