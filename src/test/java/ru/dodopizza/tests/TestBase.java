67a7c3ce4ce35ad6f8316d9c2175af42869b7471
toytronic22 <leshaklew@gmail.com>
2023-05-01 18:30:38 +0300

    pravki22

--- src/test/java/ru/dodopizza/tests/TestBase.java
+++ src/test/java/ru/dodopizza/tests/TestBase.java
@@ -1,6 +1,7 @@
 package ru.dodopizza.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
@@ -28,7 +29,7 @@ public class TestBase {
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
-        clearBrowserLocalStorage();
+        Selenide.closeWebDriver();
         closeWindow();
 
     }
