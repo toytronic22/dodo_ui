c6ce7a5f8bde4c00ea4b38f462e3c59f4865b880
toytronic22 <leshaklew@gmail.com>
2023-05-01 17:42:29 +0300

    pravki22

--- src/test/java/ru/dodopizza/helpers/Attach.java
+++ src/test/java/ru/dodopizza/helpers/Attach.java
@@ -9,25 +9,23 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
     @Attachment(value = "{attachName}", type = "image/png")
-    public static byte[] screenshotAs(String attachName) {
-        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    public static void screenshotAs(String attachName) {
+        ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
     @Attachment(value = "Page source", type = "text/plain")
-    public static byte[] pageSource() {
-        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    public static void pageSource() {
+        getWebDriver().getPageSource();
     }
 
     @Attachment(value = "{attachName}", type = "text/plain")
-    public static String attachAsText(String attachName, String message) {
-        return message;
+    public static void attachAsText(String attachName, String message) {
     }
 
     public static void browserConsoleLogs() {
@@ -39,21 +37,18 @@ public class Attach {
 
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
-    public static String addVideo() {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl()
-                + "' type='video/mp4'></video></body></html>";
+    public static void addVideo() {
+        getVideoUrl();
     }
 
-    public static URL getVideoUrl() {
+    public static void getVideoUrl() {
         String videoUrl = Configuration.remote + Selenide.sessionId() + ".mp4";
 
         try {
-            return new URL(videoUrl);
+            new URL(videoUrl);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return null;
     }
 
     public static String getSessionId(){
