d43685db2b19fa8c553926379c124d68bcb3a3bb
toytronic22 <leshaklew@gmail.com>
2023-05-01 16:42:44 +0300

    pravki

--- src/test/java/ru/dodopizza/config/web/Browser.java
+++ src/test/java/ru/dodopizza/config/web/Browser.java
@@ -1,6 +1,6 @@
 package ru.dodopizza.config.web;
 
 public class Browser {
-    public static final String CHROME = "chrome";
-    public static final String FIREFOX = "firefox";
+    public static final String chrome = "chrome";
+    public static final String firefox = "firefox";
 }
